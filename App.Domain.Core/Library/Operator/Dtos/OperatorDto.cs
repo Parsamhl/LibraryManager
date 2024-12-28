596b9cecdfb9ff3a04a6592dbc4511df31f2b9ac
parsa <mparsah$@gmail.com>
2024-12-28 15:34:28 +0330

    DataBase class created

---
+++ App.Domain.Core/Library/Operator/Dtos/OperatorDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.Library.Operator.Dtos
+{
+    public class OperatorDto
+    {
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+    }
+}
--- App.infra.DB.SqlServer/obj/Debug/net8.0/App.infra.DB.SqlServer.AssemblyInfo.cs
+++ App.infra.DB.SqlServer/obj/Debug/net8.0/App.infra.DB.SqlServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.infra.DB.SqlServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57be5e7f7b70a61ab14f6365c30f814030789ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.infra.DB.SqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.infra.DB.SqlServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
